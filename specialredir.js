8a2b765c17fd0569b75be2b6ab059cab2427957d
ethylenediaminetetraacetates <123036101+ethylenediaminetetraacetates@users.noreply.github.com>
2023-10-31 19:48:42 -0400

    again with the bfcache
    i don't understand

--- specialredir.js
+++ specialredir.js
@@ -48,7 +48,7 @@ function redirection(){
     let q = setInterval(function(){
         countdown = countdown - 1;
         document.querySelector("#loadingthingy").innerText = new URLSearchParams(document.location.search).get("site") + " in " + countdown;
-        if(countdown == 0){clearInterval(q);loadingisfun(0);countdown = 0;ready = false;}
+        if(countdown == 0){countdown = 10;ready = false;clearInterval(q);loadingisfun(0);}
     },50)
     setTimeout(function(){
         document.location = new URLSearchParams(document.location.search).get("site")
